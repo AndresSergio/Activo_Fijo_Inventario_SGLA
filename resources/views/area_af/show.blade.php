571b1cc352e0290864f6d1121c8451785f820d29
ScZJAC <jac_m_91@hotmail.com>
2021-05-24 16:54:40 -0400

    area

--- resources/views/area_af/show.blade.php
+++ resources/views/area_af/show.blade.php
@@ -7,16 +7,16 @@
 
             <div class="col-md-9">
                 <div class="card">
-                    <div class="card-header">area_af {{ $area_af->id }}</div>
+                    <div class="card-header">Área {{ $area_af->id }}</div>
                     <div class="card-body">
 
-                        <a href="{{ url('/area_af') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <a href="{{ url('/area_af/' . $area_af->id . '/edit') }}" title="Edit area_af"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                        <a href="{{ url('/area_af') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Volver</button></a>
+                        <a href="{{ url('/area_af/' . $area_af->id . '/edit') }}" title="Edit area_af"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Editar</button></a>
 
                         <form method="POST" action="{{ url('area_af' . '/' . $area_af->id) }}" accept-charset="UTF-8" style="display:inline">
                             {{ method_field('DELETE') }}
                             {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger btn-sm" title="Delete area_af" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                            <button type="submit" class="btn btn-danger btn-sm" title="Delete area_af" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Borrar</button>
                         </form>
                         <br/>
                         <br/>
@@ -24,9 +24,9 @@
                         <div class="table-responsive">
                             <table class="table">
                                 <tbody>
-                                    <tr>
+                                    <!-- <tr>
                                         <th>ID</th><td>{{ $area_af->id }}</td>
-                                    </tr>
+                                    </tr> -->
                                     <tr><th> Codigo </th><td> {{ $area_af->codigo }} </td></tr><tr><th> Nombre </th><td> {{ $area_af->nombre }} </td></tr><tr><th> Descripcion </th><td> {{ $area_af->descripcion }} </td></tr>
                                 </tbody>
                             </table>
